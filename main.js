cefb0888679fcc856b1cdac012f1a4e894928ce7
Mathias <math92k4@stud.kea.dk>
2021-05-12 11:34:23 +0200

    overblik iteration 1

--- main.js
+++ main.js
@@ -1,68 +1,121 @@
 "use strict";
 
+import "./sass/style.scss";
+
 import { getJSON } from "./rest_actions.js";
 
 window.addEventListener("DOMContentLoaded", init);
 
 async function init() {
-  let newData;
-  let oldData;
+  getData();
+  setInterval(getData, 5000);
+}
 
+async function getData() {
   const url = "https://foobarfirefjerdedele.herokuapp.com/";
-  newData = await getJSON(url);
+  const jsonData = await getJSON(url);
 
-  displayDataInit(newData);
+  // console.log(jsonData.bar);
 
-  setInterval(updateData, 5000);
+  displayBar(jsonData.bar);
+  displayQueue(jsonData.queue);
+  displayServing(jsonData.serving);
+  displayBartenders(jsonData.bartenders);
+  displayTaps(jsonData.taps);
+  displayStorage(jsonData.storage);
+}
 
-  async function updateData() {
-    oldData = newData;
-    newData = await getJSON(url);
+function displayBar(data) {
+  const container = document.querySelector("#bar");
+  container.querySelector(".name").textContent = data.name;
+  container.querySelector(".closingTime").textContent = data.closingTime;
+}
 
-    const dataToUpdate = compareData(oldData, newData);
-    console.log(dataToUpdate.length);
-    if (dataToUpdate.length > 0) {
-      updateUI(dataToUpdate);
-    }
+function displayQueue(data) {
+  const container = document.querySelector("#queue");
+  container.innerHTML = "";
+
+  if (data.length > 0) {
+    data.forEach((elm) => {
+      const temp = document.querySelector("#queue_temp");
+      const klon = temp.cloneNode(true).content;
+
+      klon.querySelector(".id").textContent = elm.id;
+      klon.querySelector(".startTime").textContent = elm.startTime;
+      klon.querySelector(".order").textContent = JSON.stringify(elm.order);
+
+      container.appendChild(klon);
+    });
   }
 }
 
-function displayDataInit(data) {
-  const peter = document.querySelector("#Peter");
-  const dannie = document.querySelector("#Dannie");
-  const jonas = document.querySelector("#Jonas");
+function displayServing(data) {
+  const container = document.querySelector("#serving");
+  container.innerHTML = "";
 
-  data.bartenders.forEach((person) => {
-    if (person.name === "Peter") {
-      peter.querySelector(".using_tap").textContent = person.usingTap;
-    }
+  if (data.length > 0) {
+    data.forEach((elm) => {
+      const temp = document.querySelector("#queue_temp");
+      const klon = temp.cloneNode(true).content;
+
+      klon.querySelector(".id").textContent = elm.id;
+      klon.querySelector(".startTime").textContent = elm.startTime;
+      klon.querySelector(".order").textContent = JSON.stringify(elm.order);
+
+      container.appendChild(klon);
+    });
+  }
+}
+
+function displayBartenders(data) {
+  data.forEach((elm) => {
+    const container = document.querySelector("#" + elm.name);
+    container.querySelector(".status").textContent = elm.status;
+    container.querySelector(".detail").textContent = elm.statusDetail;
+    container.querySelector(".serving").textContent = elm.servingCustomer;
+    container.querySelector(".using_tap").textContent = elm.usingTap;
   });
 }
 
-function compareData(oldData, newData) {
-  let result;
+function displayTaps(data) {
+  const container = document.querySelector("#taps");
+  container.innerHTML = "";
 
-  oldData.bartenders.forEach((oldObject) => {
-    result = newData.bartenders.filter(comparingBartenders);
+  data.forEach((elm) => {
+    const temp = document.querySelector("#taps_temp");
+    const klon = temp.cloneNode(true).content;
 
-    function comparingBartenders(newObject) {
-      if (JSON.stringify(oldObject) !== JSON.stringify(newObject) && newObject.name === oldObject.name) {
-        return true;
-      } else {
-        return false;
-      }
-    }
+    klon.querySelector(".id").textContent = elm.id;
+    klon.querySelector(".level").textContent = elm.level;
+    klon.querySelector(".capacity").textContent = elm.capacity;
+    klon.querySelector(".beer").textContent = elm.beer;
+    klon.querySelector(".inUse").textContent = elm.inUSe;
+    klon.querySelector("img").src = `images/${generateImgName(elm.beer)}.png`;
+
+    container.appendChild(klon);
   });
-
-  return result;
 }
 
-function updateUI(data) {
-  console.log("update");
+function displayStorage(data) {
+  const container = document.querySelector("#storage");
+  container.innerHTML = "";
 
-  data.forEach((object) => {
-    const person = document.querySelector("#" + object.name);
-    person.querySelector(".using_tap").textContent = object.usingTap;
-    console.log(object.usingTap);
+  data.forEach((elm) => {
+    const temp = document.querySelector("#storage_temp");
+    const klon = temp.cloneNode(true).content;
+
+    klon.querySelector(".name").textContent = elm.name;
+    klon.querySelector(".amount").textContent = elm.amount;
+
+    klon.querySelector("img").src = `images/${generateImgName(elm.name)}.png`;
+
+    container.appendChild(klon);
   });
+}
+
+function generateImgName(beerName) {
+  const noSpace = beerName.replaceAll(" ", "");
+  const lowerCased = noSpace.toLowerCase();
+
+  return lowerCased;
 }
